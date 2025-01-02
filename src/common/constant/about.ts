cd00ec01e4426aa3c60e22289bccc3db0ce8f97f
Swanand-Wagh <swanandwagh7@gmail.com>
2025-01-01 22:09:55 -0800

    misc

--- src/common/constant/about.ts
+++ src/common/constant/about.ts
@@ -6,7 +6,7 @@ export const ABOUT = `
 	In terms of technical skills, I specialize in a range of programming languages and frameworks, including React (Next.js, Vite), TypeScript, JavaScript, Node.js, and Azure (Docker, Kubernetes). 
 </p>
 <p>
-	I started my company at the age of 18, co-founding SayItSocial Pvt. Ltd. Our idea was to make an Instagram for NGOs, fostering collaboration and enabling them to share campaigns directly with citizens. To encourage citizen engagement, we implemented a reward system by awarding virtual points redeemable later.
+	My friends & I started my company at the age of 18, co-founding SayItSocial Pvt. Ltd. Our idea was to make an Instagram for NGOs, fostering collaboration and enabling them to share campaigns directly with citizens. To encourage citizen engagement, we implemented a reward system by awarding virtual points redeemable later.
 </p>
 <p>
 	 Additionally, my another startup Greenie tried to address background verification challenges through its innovative BGV web app. Since its launch, Greenie has gained considerable traction, with over 4000 individuals utilizing the platform. These ventures underscore my dedication to leveraging technology for community-driven initiatives and making a positive impact.
